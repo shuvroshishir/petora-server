896c0743467d144788125402ac34d609ea696439
shuvroshishir <shuvroshishirx@gmail.com>
2026-05-20 19:36:12 +0600

    MongoDB Server Connected

--- index.js
+++ index.js
@@ -2,10 +2,50 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+// importing necessary modules
 const express = require('express')
-const app = express()
-const port = 5000
+const dotenv = require('dotenv')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
+// initializing express app and dotenv
+const app = express()
+dotenv.config()
+
+// setting up port and uri from environment variables
+const port = process.env.PORT;
+const uri = process.env.MONGO_DB_URI;
+
+
+app.use(cors())
+app.use(express.json())
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
